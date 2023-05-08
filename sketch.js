d2bf15b3922ecfdd32b909b013ab92bd2517f95e
DiogoChernicciaro <128418593+DiogoChernicciaro@users.noreply.github.com>
2023-05-08 16:06:21 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+      pathImg = loadImage("path.png");
+    boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+ path = createSprite(200,400,100,100);
+path.addImage("rua", pathImg);
+path.velocityY = 4
+path.scale=1.2;
+
+boy = createSprite(20,100,20,20);
+boy.addAnimation("corrida",boyImg);
+boy.scale=0.08;
+  
+//crie um limite à esquerda
+leftBoundary=createSprite(0,0,100,800);
+//defina visibilidade como falsa para o limite à esquerda
+leftBoundary.visibility = false;
+//crie um limite à direita
+rightBoundary=createSprite(410,0,100,800);
+//defina visibilidade como falsa para o limite à direita
+rightBoundary.visibility = false;
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  // mover o menino com o mouse usando mouseX
+  boy.x=World.mouseX;
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  // colidir o menino com os limites invisíveis da esquerda e da direita
+  boy.collide(rightBoundary);
+  boy.collide(leftBoundary);
+  //código para redefinir o fundo
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+  drawSprites();
+}
